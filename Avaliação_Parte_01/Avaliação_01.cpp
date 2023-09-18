df3725ab728a12a49e7cc88784f9cc1128d10cb8
WiltonLio <wilton.lio@gmail.com>
2023-09-18 10:32:15 -0300

    ADICIONADO novo arquivo Avaliação_01.cpp
    - Adicionado código em C++ para In bibliotecas para usar strings, vetor e entrada/saída.
    - Criada a função principal para o programa.

---
+++ Avaliação_Parte_01/Avaliação_01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Passageiro
+{
+    string cpf;
+    string nome;
+    string dtNascimento;
+    string autorizacao;
+};
+    int main() 
+{
+    
+}
